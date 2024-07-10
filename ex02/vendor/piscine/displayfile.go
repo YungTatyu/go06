790d8630e3b3f0b64dc894d3b8877cef5ad2cfd4
Tatsuki Terao <tterao@c3r2s4.42tokyo.jp>
2024-07-10 14:06:26 +0900

    :recycle:refactor: ex02

--- ex02/vendor/piscine/displayfile.go
+++ ex02/vendor/piscine/displayfile.go
@@ -13,22 +13,24 @@ var argc = func() int {
 	return length
 }()
 
-func DisplayFile() {
+func DisplayFile() int {
 	if argc == 1 {
 		os.Stderr.WriteString("File name missing\n")
-		return
+		return 1
 	}
 	if argc != 2 {
 		os.Stderr.WriteString("Too many arguments\n")
-		return
+		return 1
 	}
 	data, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		os.Stderr.WriteString("failed to open a file\n")
-		return
+		return 1
 	}
 	_, err = os.Stdout.Write(data)
 	if err != nil {
 		os.Stderr.WriteString("failed to write to stdout: " + err.Error() + "\n")
+		return 1
 	}
+	return 0
 }
